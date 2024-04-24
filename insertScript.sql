4d8a7ab83f231364761e39755c42537e1167c2d2
nopa <110712245+Nopathee@users.noreply.github.com>
2024-04-24 21:45:19 +0700

    Update insertScript.sql

--- insertScript.sql
+++ insertScript.sql
@@ -5,7 +5,7 @@ INSERT INTO tasks (task_title, task_description, task_assignees, task_status)
 VALUES ('Conduct market research', 'Gather data and analyze market trends for the project', 'David Wilson', 'Doing');
 
 INSERT INTO tasks (task_title, task_description, task_assignees, task_status)
-VALUES ('Finalize project milestones', 'Review and confirm project milestones with stakeholders', 'Sophia Lee', 'NoStatus');
+VALUES ('Finalize project milestones', 'Review and confirm project milestones with stakeholders', 'Sophia Lee', 'No_Status');
 
 INSERT INTO tasks (task_title, task_description, task_assignees, task_status)
 VALUES ('Test project prototype', 'Conduct testing on the project prototype for quality assurance', 'Daniel Martinez', 'Doing');
