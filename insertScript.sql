2f9249cc18e3f40d3ae5977482aabd2f618f34e4
nopa <110712245+Nopathee@users.noreply.github.com>
2024-04-25 21:44:45 +0700

    Update insertScript.sql

--- insertScript.sql
+++ insertScript.sql
@@ -1,11 +1,18 @@
+-- row1
 INSERT INTO tasks (task_title, task_description, task_assignees, task_status)
-VALUES ('Prepare project presentation', 'Create slides and prepare content for project presentation', 'Emily Brown', 'To_Do');
+VALUES ('TaskTitle1TaskTitle2TaskTitle3TaskTitle4TaskTitle5TaskTitle6TaskTitle7TaskTitle8TaskTitle9TaskTitle0',
+ 'Descripti1Descripti2Descripti3Descripti4Descripti5Descripti6Descripti7Descripti8Descripti9Descripti1Descripti1Descripti2Descripti3Descripti4Descripti5Descripti6Descripti7Descripti8Descripti9Descripti2Descripti1Descripti2Descripti3Descripti4Descripti5Descripti6Descripti7Descripti8Descripti9Descripti3Descripti1Descripti2Descripti3Descripti4Descripti5Descripti6Descripti7Descripti8Descripti9Descripti4Descripti1Descripti2Descripti3Descripti4Descripti5Descripti6Descripti7Descripti8Descripti9Descripti5',
+ 'Assignees1Assignees2Assignees3',
+ 'No Status');
 
+-- row2
 INSERT INTO tasks (task_title, task_description, task_assignees, task_status)
-VALUES ('Conduct market research', 'Gather data and analyze market trends for the project', 'David Wilson', 'Doing');
+VALUES ('Repository', '', '', 'To Do');
 
+-- row3
 INSERT INTO tasks (task_title, task_description, task_assignees, task_status)
-VALUES ('Finalize project milestones', 'Review and confirm project milestones with stakeholders', 'Sophia Lee', 'No_Status');
+VALUES ('ดาต้าเบส', 'ສ ້າງຖານຂໍມ ູນ', 'あなた、彼、彼女 (私ではありません)', 'Doing');
 
+-- row4
 INSERT INTO tasks (task_title, task_description, task_assignees, task_status)
-VALUES ('Test project prototype', 'Conduct testing on the project prototype for quality assurance', 'Daniel Martinez', 'Doing');
+VALUES (' _Infrastructure_ ', ' _Setup containers_ ', 'ไก่งวง กับ เพนกวิน', 'Done');
