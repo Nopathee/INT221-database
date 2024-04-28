4e732c5e52b6d539327f3ab93fc137d375fe643a
nopa <110712245+Nopathee@users.noreply.github.com>
2024-04-29 00:20:58 +0700

    Update taskscript.sql

--- taskscript.sql
+++ taskscript.sql
@@ -32,7 +32,7 @@ CREATE TABLE `tasks` (
   `task_id` int NOT NULL AUTO_INCREMENT,
   `task_title` varchar(100) NOT NULL,
   `task_description` varchar(500) DEFAULT NULL,
-  `task_assignees` varchar(30) DEFAULT NULL,
+  `task_assignees` tinytext DEFAULT NULL,
   `task_status` enum('No Status','To Do','Doing','Done') NOT NULL,
   `created_on` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
   `updated_on` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
@@ -60,8 +60,46 @@ UNLOCK TABLES;
 
 -- Dump completed on 2024-04-23 21:27:08
 
-LOCK TABLES `tasks` WRITE;
-/*!40000 ALTER TABLE `tasks` DISABLE KEYS */;
-INSERT INTO `tasks` VALUES (1,'TaskTitle1TaskTitle2TaskTitle3TaskTitle4TaskTitle5TaskTitle6TaskTitle7TaskTitle8TaskTitle9TaskTitle0','Descripti1Descripti2Descripti3Descripti4Descripti5Descripti6Descripti7Descripti8Descripti9Descripti1Descripti1Descripti2Descripti3Descripti4Descripti5Descripti6Descripti7Descripti8Descripti9Descripti2Descripti1Descripti2Descripti3Descripti4Descripti5Descripti6Descripti7Descripti8Descripti9Descripti3Descripti1Descripti2Descripti3Descripti4Descripti5Descripti6Descripti7Descripti8Descripti9Descripti4Descripti1Descripti2Descripti3Descripti4Descripti5Descripti6Descripti7Descripti8Descripti9Descripti5','Assignees1Assignees2Assignees3','NO STATUS','2024-04-22 09:00:00','2024-04-22 09:00:00'),(2,'Repository','','','TO DO','2024-04-22 09:05:00','2024-04-22 14:00:00'),(3,'ดาต้าเบส','ສ້າງຖານຂໍ້ມູນ','あなた、彼、彼女 (私ではありません)','DOING','2024-04-22 09:10:00','2024-04-25 00:00:00'),(4,'_Infrastructure_','_Setup containers_','ไก่งวง กับ เพนกวิน','DONE','2024-04-22 09:15:00','2024-04-22 10:00:00');
-/*!40000 ALTER TABLE `tasks` ENABLE KEYS */;
-UNLOCK TABLES;
+-- Row 1
+INSERT INTO tasks (task_title, task_description, task_assignees, task_status,created_on, updated_on)
+VALUES (
+    'TaskTitle1TaskTitle2TaskTitle3TaskTitle4TaskTitle5TaskTitle6TaskTitle7TaskTitle8TaskTitle9TaskTitle0',
+      'Descripti1Descripti2Descripti3Descripti4Descripti5Descripti6Descripti7Descripti8Descripti9Descripti1Descripti1Descripti2Descripti3Descripti4Descripti5Descripti6Descripti7Descripti8Descripti9Descripti2Descripti1Descripti2Descripti3Descripti4Descripti5Descripti6Descripti7Descripti8Descripti9Descripti3Descripti1Descripti2Descripti3Descripti4Descripti5Descripti6Descripti7Descripti8Descripti9Descripti4Descripti1Descripti2Descripti3Descripti4Descripti5Descripti6Descripti7Descripti8Descripti9Descripti5',
+    'Assignees1Assignees2Assignees3',
+    'No Status',
+ '2024-04-22 16:00:00+07:00',
+ '2024-04-22 16:00:00+07:00'
+);
+
+-- Row 2
+INSERT INTO tasks (task_title, task_description, task_assignees, task_status,created_on, updated_on)
+VALUES (
+    'Repository',
+    '',
+    '',
+    'To Do',
+ '2024-04-22 16:05:00+07:00',
+ '2024-04-22 21:00:00+07:00'
+);
+
+-- Row 3
+INSERT INTO tasks (task_title, task_description, task_assignees, task_status,created_on, updated_on)
+VALUES (
+    'ดาต้าเบส',
+    'ສ້າງຖານຂໍ້ມູນ',
+    'あなた、彼、彼女 (私ではありません)',
+    'Doing',
+ '2024-04-22 16:10:00+07:00',
+ '2024-04-25 07:00:00+07:00'
+);
+
+-- Row 4
+INSERT INTO tasks (task_title, task_description, task_assignees, task_status,created_on, updated_on)
+VALUES (
+    '_Infrastructure_',
+    '_Setup containers_',
+    'ไก่งวง กับ เพนกวิน',
+    'Done',
+ '2024-04-22 16:15:00+07:00',
+ '2024-04-22 17:00:00+07:00'
+);
