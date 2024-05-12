ffade018ae6dea2ab414ee1c1d7114870cec023f
nopa <110712245+Nopathee@users.noreply.github.com>
2024-05-12 19:06:36 +0700

    Update taskscript.sql

--- taskscript.sql
+++ taskscript.sql
@@ -1,5 +1,7 @@
 DROP TABLE IF EXISTS `tasksV2`;
 DROP TABLE IF EXISTS `status`;
+DROP TABLE IF EXISTS `tasks`;
+
 CREATE TABLE `status` (
 `id` int NOT NULL AUTO_INCREMENT PRIMARY KEY,
 `statusName` VARCHAR(50) NOT NULL UNIQUE,
@@ -8,7 +10,6 @@ CREATE TABLE `status` (
 `updated_on` datetime DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-
 
 CREATE TABLE `tasksV2` (
   `id` int NOT NULL AUTO_INCREMENT,
@@ -21,6 +22,17 @@ CREATE TABLE `tasksV2` (
   PRIMARY KEY (`id`),
   FOREIGN KEY (`task_status_id`) REFERENCES `status`(`id`) 
 ) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+CREATE TABLE `tasks` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `task_title` varchar(100) NOT NULL,
+  `task_description` varchar(500) DEFAULT NULL,
+  `task_assignees` tinytext DEFAULT NULL,
+  `task_status` enum('NO_STATUS','TO_DO','DOING','DONE') DEFAULT 'NO_STATUS',
+  `created_on` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `updated_on` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 
 INSERT INTO `status` (`statusName`, `statusDescription`)
 VALUES 
@@ -47,64 +59,25 @@ VALUES
 '2024-04-22 09:15:00',
 '2024-04-22 10:00:00');
 
--- MySQL dump 10.13  Distrib 8.3.0, for macos14.2 (arm64)
---
--- Host: localhost    Database: SSI3
--- ------------------------------------------------------
--- Server version	8.3.0
-
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!50503 SET NAMES utf8mb4 */;
-/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
-/*!40103 SET TIME_ZONE='+00:00' */;
-/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
-/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
-/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
-/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
-
---
--- Table structure for table `tasks`
--- tasks
-
-DROP TABLE IF EXISTS `tasks`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `tasks` (
-  `id` int NOT NULL AUTO_INCREMENT,
-  `task_title` varchar(100) NOT NULL,
-  `task_description` varchar(500) DEFAULT NULL,
-  `task_assignees` tinytext DEFAULT NULL,
-  `task_status` enum('NO_STATUS','TO_DO','DOING','DONE') DEFAULT 'NO_STATUS',
-  `created_on` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
-  `updated_on` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `tasks`
---
-
-LOCK TABLES `tasks` WRITE;
-/*!40000 ALTER TABLE `tasks` DISABLE KEYS */;
-INSERT INTO `tasks` VALUES (1,'TaskTitle1TaskTitle2TaskTitle3TaskTitle4TaskTitle5TaskTitle6TaskTitle7TaskTitle8TaskTitle9TaskTitle0','Descripti1Descripti2Descripti3Descripti4Descripti5Descripti6Descripti7Descripti8Descripti9Descripti1Descripti1Descripti2Descripti3Descripti4Descripti5Descripti6Descripti7Descripti8Descripti9Descripti2Descripti1Descripti2Descripti3Descripti4Descripti5Descripti6Descripti7Descripti8Descripti9Descripti3Descripti1Descripti2Descripti3Descripti4Descripti5Descripti6Descripti7Descripti8Descripti9Descripti4Descripti1Descripti2Descripti3Descripti4Descripti5Descripti6Descripti7Descripti8Descripti9Descripti5','Assignees1Assignees2Assignees3','NO_STATUS','2024-04-22 09:00:00','2024-04-22 09:00:00'),(2,'Repository','','','TO_DO','2024-04-22 09:05:00','2024-04-22 14:00:00'),(3,'ดาต้าเบส','ສ້າງຖານຂໍ້ມູນ','あなた、彼、彼女 (私ではありません)','DOING','2024-04-22 09:10:00','2024-04-25 00:00:00'),(4,'_Infrastructure_','_Setup containers_','ไก่งวง กับ เพนกวิน','DONE','2024-04-22 09:15:00','2024-04-22 10:00:00');
-/*!40000 ALTER TABLE `tasks` ENABLE KEYS */;
-UNLOCK TABLES;
-/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
-
-/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
-/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
-/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
-/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
-
--- Dump completed on 2024-04-29 18:40:39
-
-
+INSERT INTO `tasks` VALUES (
+1,'TaskTitle1TaskTitle2TaskTitle3TaskTitle4TaskTitle5TaskTitle6TaskTitle7TaskTitle8TaskTitle9TaskTitle0',
+'Descripti1Descripti2Descripti3Descripti4Descripti5Descripti6Descripti7Descripti8Descripti9Descripti1Descripti1Descripti2Descripti3Descripti4Descripti5Descripti6Descripti7Descripti8Descripti9Descripti2Descripti1Descripti2Descripti3Descripti4Descripti5Descripti6Descripti7Descripti8Descripti9Descripti3Descripti1Descripti2Descripti3Descripti4Descripti5Descripti6Descripti7Descripti8Descripti9Descripti4Descripti1Descripti2Descripti3Descripti4Descripti5Descripti6Descripti7Descripti8Descripti9Descripti5',
+'Assignees1Assignees2Assignees3',
+'NO_STATUS',
+'2024-04-22 09:00:00',
+'2024-04-22 09:00:00'),
+(2,'Repository','','',
+'TO_DO','2024-04-22 09:05:00',
+'2024-04-22 14:00:00'),
+(3,'ดาต้าเบส','ສ້າງຖານຂໍ້ມູນ',
+'あなた、彼、彼女 (私ではありません)',
+'DOING','2024-04-22 09:10:00',
+'2024-04-25 00:00:00'),
+(4,'_Infrastructure_',
+'_Setup containers_',
+'ไก่งวง กับ เพนกวิน','DONE',
+'2024-04-22 09:15:00',
+'2024-04-22 10:00:00');
 
 commit;
 
